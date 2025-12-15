bc5951f10a5242f84801c91c71b83a8b2eb6a200
Zx-Aaru <armaanmansuri786@yahoo.com>
2025-12-15 09:00:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lajabajwkjsaiajajahjwkdhdywhjwjsjsgs/Mc",
+    "https://github.com/Zx-Aaru/PublicHost",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
